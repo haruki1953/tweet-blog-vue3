65e235201bf055ee4c6d285443d85e1a7e0ad567
haruki1953 <kinikun@outlook.com>
2025-01-31 19:03:23 +0800

    项目版本完善

--- src/config/info.ts
+++ src/config/info.ts
@@ -4,7 +4,7 @@ export const appInfo = {
     link: 'https://github.com/haruki1953/Tweblog'
   },
   version: {
-    text: '0.0.2',
+    text: '1.0.0',
     link: 'https://github.com/haruki1953/Tweblog'
   },
   officialWebsite: {
